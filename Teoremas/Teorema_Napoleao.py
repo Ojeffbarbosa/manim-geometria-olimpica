2165364e1968c82bf5de08ce88a504c9ffe9739c
Ojeffbarbosa <jefferson.barbosa.lima@aluno.uepb.edu.br>
2026-05-30 18:12:50 -0300

    Refactor Napoleon Theorem visualization with external triangles
    Updated the implementation of the Napoleon Theorem visualization to include external triangles and adjusted color definitions.

--- Teoremas/Teorema_Napoleao.py
+++ Teoremas/Teorema_Napoleao.py
@@ -2,14 +2,16 @@
 TEOREMA DE NAPOLEÃO
 
 O Teorema de Napoleão afirma que:
-Se triângulos equiláteros são construídos sobre os lados de qualquer triângulo, então os centros desses 
-triângulos equiláteros formam um triângulo equilátero.
+	Seja $ABC$ um triângulo qualquer. Construam-se externamente sobre os lados $\overline{BC}$, 
+    $\overline{CA}$ e $\overline{AB}$ os triângulos equiláteros $\triangle BCA'$, $\triangle CAB'$ e
+    $\triangle ABC'$, respectivamente. Sejam $P$, $Q$ e $R$ os centros de $\triangle BCA'$, $\triangle CAB'$ 
+    e $\triangle ABC'$, respectivamente. Então o triângulo $PQR$ é equilátero.
 
 
 Elementos da construção:
 - ABC: triângulo original
-- D, E, F: vértices externos dos triângulos equiláteros construídos sobre AB, BC e CA
-- P, Q, R: centros dos triângulos equiláteros ABD, BCE e CAF
+- A', B', C': vértices externos dos triângulos equiláteros construídos sobre BC, CA e AB
+- P, Q, R: centros dos triângulos equiláteros BCA', CAB' e ABC'
 - Triângulo PQR: Triângulo de Napoleão
 
 Data: 15/11/2025
@@ -29,9 +31,9 @@ class NapoleonTheorem(Scene):
         COR_FILL_BASE = GRAY
         
         #cores dos triangulos equiláteros externos
-        COR_ABD = YELLOW  # Triângulo sobre o lado AB
-        COR_BCE = BLUE    # Triângulo sobre o lado BC
-        COR_CAF = GREEN   # Triângulo sobre o lado CA
+        COR_ABC_EXT = YELLOW  # Triângulo sobre o lado AB (ABC')
+        COR_BCA_EXT = BLUE    # Triângulo sobre o lado BC (BCA')
+        COR_CAB_EXT = GREEN   # Triângulo sobre o lado CA (CAB')
         
         #cores do triangulo de Napoleão PQR
         COR_BORDA_NAP = BLACK
@@ -165,13 +167,13 @@ class NapoleonTheorem(Scene):
             return tri_temp.get_center_of_mass()
         
         # Funções específicas para cada centro
-        def get_P():  # Centro do triângulo sobre AB
+        def get_P():  # Centro oposto ao vértice A visual (sobre a aresta A-B interna do código que liga VisB a VisC)
             return get_centro(get_vert_A(), get_vert_B(), get_vert_C())
         
-        def get_Q():  # Centro do triângulo sobre BC
+        def get_Q():  # Centro oposto ao vértice B visual (sobre a aresta B-C interna do código que liga VisC a VisA)
             return get_centro(get_vert_B(), get_vert_C(), get_vert_A())
         
-        def get_R():  # Centro do triângulo sobre CA
+        def get_R():  # Centro oposto ao vértice C visual (sobre a aresta C-A interna do código que liga VisA a VisB)
             return get_centro(get_vert_C(), get_vert_A(), get_vert_B())
         
         # CRIAÇÃO DOS ELEMENTOS VISUAIS COM UPDATERS
@@ -188,35 +190,35 @@ class NapoleonTheorem(Scene):
             stroke_width=4
         ))
         
-        # Triângulo equilátero ABD (sobre o lado AB - amarelo)
-        tri_ABD = always_redraw(lambda: Polygon(
+        # Triângulo equilátero ABC' (sobre o lado AB - amarelo)
+        tri_ABC_ext = always_redraw(lambda: Polygon(
             get_vert_A(), 
             get_vert_B(),
             construir_tri_eq_externo(get_vert_A(), get_vert_B(), get_vert_C()),
             color=COR_BORDA,
-            fill_color=COR_ABD,
+            fill_color=COR_ABC_EXT,
             fill_opacity=0.25,
             stroke_width=3
         ))
         
-        # Triângulo equilátero BCE (sobre o lado BC - azul)
-        tri_BCE = always_redraw(lambda: Polygon(
+        # Triângulo equilátero BCA' (sobre o lado BC - azul)
+        tri_BCA_ext = always_redraw(lambda: Polygon(
             get_vert_B(), 
             get_vert_C(),
             construir_tri_eq_externo(get_vert_B(), get_vert_C(), get_vert_A()),
             color=COR_BORDA,
-            fill_color=COR_BCE,
+            fill_color=COR_BCA_EXT,
             fill_opacity=0.25,
             stroke_width=3
         ))
         
-        # Triângulo equilátero CAF (sobre o lado CA - verde)
-        tri_CAF = always_redraw(lambda: Polygon(
+        # Triângulo equilátero CAB' (sobre o lado CA - verde)
+        tri_CAB_ext = always_redraw(lambda: Polygon(
             get_vert_C(), 
             get_vert_A(),
             construir_tri_eq_externo(get_vert_C(), get_vert_A(), get_vert_B()),
             color=COR_BORDA,
-            fill_color=COR_CAF,
+            fill_color=COR_CAB_EXT,
             fill_opacity=0.25,
             stroke_width=3
         ))
@@ -226,9 +228,9 @@ class NapoleonTheorem(Scene):
 
         
         # Pontos nos vértices do triângulo original
-        pt_A = always_redraw(lambda: Dot(get_vert_A(), color=COR_TXT, radius=0.06))
-        pt_B = always_redraw(lambda: Dot(get_vert_B(), color=COR_TXT, radius=0.06))
-        pt_C = always_redraw(lambda: Dot(get_vert_C(), color=COR_TXT, radius=0.06))
+        pt_B = always_redraw(lambda: Dot(get_vert_A(), color=COR_TXT, radius=0.06))
+        pt_C = always_redraw(lambda: Dot(get_vert_B(), color=COR_TXT, radius=0.06))
+        pt_A = always_redraw(lambda: Dot(get_vert_C(), color=COR_TXT, radius=0.06))
         
         # Pontos nos centros dos triângulos equiláteros 
         pt_P = always_redraw(lambda: Dot(get_P(), color=COR_TXT, radius=0.08))
@@ -307,16 +309,16 @@ class NapoleonTheorem(Scene):
         ))
         
         # Rótulos dos vértices do triângulo original
-        rot_A = always_redraw(lambda: MathTex(
-            "A", font_size=36, color=COR_TXT
-        ).next_to(get_vert_A(), DOWN, buff=0.2))
-        
         rot_B = always_redraw(lambda: MathTex(
             "B", font_size=36, color=COR_TXT
-        ).next_to(get_vert_B(), DOWN, buff=0.2))
+        ).next_to(get_vert_A(), DOWN, buff=0.2))
         
         rot_C = always_redraw(lambda: MathTex(
             "C", font_size=36, color=COR_TXT
+        ).next_to(get_vert_B(), DOWN, buff=0.2))
+        
+        rot_A = always_redraw(lambda: MathTex(
+            "A", font_size=36, color=COR_TXT
         ).next_to(get_vert_C(), UP, buff=0.2))
         
         # Rótulos dos centros (vértices do Triângulo de Napoleão)
@@ -335,17 +337,17 @@ class NapoleonTheorem(Scene):
   
         # --- ETAPA 1: CONSTRUIR O TRIÂNGULO ORIGINAL ABC 
         self.play(Create(tri_ABC))
-        self.play(FadeIn(pt_A), FadeIn(pt_B), FadeIn(pt_C))
-        self.add(rot_A, rot_B, rot_C)
-        self.play(Write(rot_A), Write(rot_B), Write(rot_C))
+        self.play(FadeIn(pt_B), FadeIn(pt_C), FadeIn(pt_A))
+        self.add(rot_B, rot_C, rot_A)
+        self.play(Write(rot_B), Write(rot_C), Write(rot_A))
         self.wait()
         
         # --- ETAPA 2: CONSTRUIR OS TRIÂNGULOS EQUILÁTEROS EXTERNOS 
-        self.play(Create(tri_ABD))
+        self.play(Create(tri_ABC_ext))
         self.wait(0.3)
-        self.play(Create(tri_BCE))
+        self.play(Create(tri_BCA_ext))
         self.wait(0.3)
-        self.play(Create(tri_CAF))
+        self.play(Create(tri_CAB_ext))
         self.wait()
         
         # --- ETAPA 3: MOSTRAR OS CENTROS P, Q, R 
